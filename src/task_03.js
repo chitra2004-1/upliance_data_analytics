a36ee48706805eb627354b7ddef86ed91e17f57d
chitra2004-1 <chitraprabhu737@gmail.com>
2026-01-10 18:54:40 +0530

    Update task_03.js

--- src/task_03.js
+++ src/task_03.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const dataPath = path.join(__dirname, '../public/data.json');
+const dataPath = path.join(__dirname, '../data.json');
 const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 
 const granularity = process.argv[2] || 'Weekly';
